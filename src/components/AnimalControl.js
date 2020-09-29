309d7133c5782ff24ec69acfe7542be13720b8d4
Fatma <fceyda@gmail.com>
2020-09-29 09:49:50 -0700

    create makeaipcall method

--- src/components/AnimalControl.js
+++ src/components/AnimalControl.js
@@ -14,14 +14,57 @@ class AnimalControl extends React.Component {
     };
   }
 
+  makeApiCall = () => {
+    fetch("http://localhost:5000/api/v1/animals/")
+    .then(response => response.json())
+    .then(
+        (jsonifiedResponse) => {
+          console.log(jsonifiedResponse);
+          if (jsonifiedResponse.status === 200 && jsonifiedResponse.ok) {
+            this.setState({
+              isLoaded: true,
+              animals: jsonifiedResponse.results
+            });
+          } else {
+            return false;
+          }
+        })
+    .catch((error) => {
+      this.setState({
+        isLoaded: true,
+        error
+      });
+    })
+  }
+
+  componentDidMount() {
+    this.makeApiCall();
+  }
+
+
   render() {
-    return (
-      <React.Fragment>
-        <AnimalList />
-        <AnimalDetails />
-        <AnimalEdit />
-      </React.Fragment>
-    )
+    const {error, isLoaded, animals} = this.state;
+    if(error) {
+      return <React.Fragment> Error: {error.message} </React.Fragment>
+    } else if (!isLoaded) {
+      return <React.Fragment>Loading...</React.Fragment>;
+    } else {
+      return (
+        <React.Fragment>
+          <ul>
+          {animals.map((animal, index) => 
+            <li key={index}>
+              <h3>{animal.name}</h3>
+              {/* <p>type: {animals.type}</p>
+              <p>age :{animals.age}</p>
+              <p>gender: {animals.gender}</p> */}
+            </li>
+          )}
+          </ul>
+        </React.Fragment>
+      )
+    }
+ 
   }
 }
 
